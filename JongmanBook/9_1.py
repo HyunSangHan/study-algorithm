71b475d24a04433ddb4dafea2413f6d780813b54
HyunSangHan <gustkd3@gmail.com>
2019-10-31 13:38:10 +0900

    Change codes to easily read 백준 12015 문제와 비슷한 형태로 코드 수정

--- JongmanBook/9_1.py
+++ JongmanBook/9_1.py
@@ -2,77 +2,41 @@ def lis(start):
     if cache[start] != -1:
         return cache[start]
     cache[start] = 1
-    bestNext = -1
-    for next in range(start, n):
-        if start == -1 or arr[start] < arr[next]:
-            cand = lis(next) + 1
-            if cand > cache[start]:
-                cache[start] = cand
-                bestNext = next
-    choices[start] = bestNext
+    bestNext = -1 ##diff
+    for next in range(start, l):
+        if arr[start] < arr[next]:
+            if cache[start] < lis(next) + 1:
+                cache[start] = lis(next) + 1
+                bestNext = next ##diff
+        print("   --- next : {}".format(next))
+        print("   | <cache> : {}".format(cache))
+        print("   | <choices> : {}".format(choices))
+
+    choices[start] = bestNext ##diff
     return cache[start]
 
-def reconstruct(start):
-    if start != -1:
-        seq_tmp.append(arr[start])
+def reconstruct(start): ##diff
+    seq_tmp.append(arr[start])
     next = choices[start]
     if next != -1:
         reconstruct(next)
 
+l = int(input())
 arr = list(map(int, input().split()))
-n = len(arr)
-cache = [-1] * n
-choices = [0] * n
-seq = []
+cache = [-1] * l
+choices = [0] * l ##diff
+seq = [] ##diff
 
-for i in range(n):
-    lis(i)
+for i in range(l):
     print("---{}---".format(i))
     print("<cache> : {}".format(cache))
     print("<choices> : {}".format(choices))
+    lis(i)
 
-for i in range(n):
+for i in range(l): ##diff
     seq_tmp = []
     reconstruct(i)
     if len(seq) < len(seq_tmp):
         seq = seq_tmp
 
 print(seq)
-
-### 테스트 결과 예시 1
-# 3 2 1 4 5
-# ---0---
-# <cache> : [3, -1, -1, 2, 1]
-# <choices> : [3, 0, 0, 4, -1]
-# ---1---
-# <cache> : [3, 3, -1, 2, 1]
-# <choices> : [3, 3, 0, 4, -1]
-# ---2---
-# <cache> : [3, 3, 3, 2, 1]
-# <choices> : [3, 3, 3, 4, -1]
-# ---3---
-# <cache> : [3, 3, 3, 2, 1]
-# <choices> : [3, 3, 3, 4, -1]
-# ---4---
-# <cache> : [3, 3, 3, 2, 1]
-# <choices> : [3, 3, 3, 4, -1]
-# [3, 4, 5]
-
-### 테스트 결과 예시 2
-# 5 4 2 3 6
-# ---0---
-# <cache> : [2, -1, -1, -1, 1]
-# <choices> : [4, 0, 0, 0, -1]
-# ---1---
-# <cache> : [2, 2, -1, -1, 1]
-# <choices> : [4, 4, 0, 0, -1]
-# ---2---
-# <cache> : [2, 2, 3, 2, 1]
-# <choices> : [4, 4, 3, 4, -1]
-# ---3---
-# <cache> : [2, 2, 3, 2, 1]
-# <choices> : [4, 4, 3, 4, -1]
-# ---4---
-# <cache> : [2, 2, 3, 2, 1]
-# <choices> : [4, 4, 3, 4, -1]
-# [2, 3, 6]
